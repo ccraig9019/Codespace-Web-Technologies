1f7a11923b8f1bc6fc9cf137c4894c3401f32712
Calum Craig <calumjamescraig@gmail.com>
2024-09-10 15:29:27 +0100

    first commit

---
+++ HTML/products.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+	<style>
+		.card{
+			margin-bottom:20px;
+			margin-top:20px;
+		}
+	</style>
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Get the shirts</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home </a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="products.php">Products <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link" href="sign_in.php">Sign in</a>
+      </li>
+	  <li class="nav-item dropdown">
+        <a class="nav-link" href="new_customer.php">New customer</a>
+      </li>
+     
+  </div>
+</nav>
+
+<div class="container">
+  <div class="row">
+	<div class="col-sm">
+		<div class="card" style="width: 20rem;">
+  			<img src="img/vulture-t.jpg" class="card-img-top" alt="Plain black t-shirt">
+			<div class="card-body">
+				<h5 class="card-title">Vulture</h5>
+				<p class="card-text">Hand-pulled screen printed onto Gildan Premium shirts.<br>Artwork by [...].<br>Available in sizes S-XXL.</p>
+				<button type="button" class="btn btn-dark">See more</button>
+			</div>
+		</div>
+	</div>
+
+
+	<div class="col-sm">
+		<div class="card" style="width: 20rem;">
+  			<img src="img/black-logo-t.jpg" class="card-img-fluid" alt="Plain red t-shirt">
+			<div class="card-body">
+				<h5 class="card-title">Black logo</h5>
+				<p class="card-text">Hand-pulled screen printed onto Gildan Premium shirts<br>Available in sizes S-XXL.</p>
+				<button type="button" class="btn btn-dark">See more</button>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-sm">
+		<div class="card" style="width: 20rem;">
+  			<img src="img/white-logo-t.jpg" class="card-img-top" alt="Plain white t-shirt">
+			<div class="card-body">
+				<h5 class="card-title">White logo</h5>
+				<p class="card-text">Hand-pulled screen printed onto Gildan Premium shirts<br>Available in sizes S-XXL.</p>
+				<button type="button" class="btn btn-dark">See more</button>
+			</div>
+		</div>
+	</div>
+
+  </div><!-- closing row -->
+</div><!-- closing container -->
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
---
+++ PHP/Workshop 1/php_test.php
@@ -0,0 +1,16 @@
+
+
+ 
+<?php
+
+//Numeric Array
+# Create and initialise an array variable.
+echo"<h2>Seasons of the Year</h2>";
+$seasons = array( 'Winter' , 'Spring' , 'Summer' , 'Autumn' ) ;
+
+# Display the value in all elements as a bulleted list.
+foreach( $seasons as $value ) 
+{ echo " $value " ; } 
+
+?>
+
